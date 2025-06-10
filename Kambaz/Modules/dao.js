ebfbaee0413b673c3028c483cfd0ae94a1d1391e
Ruby Mason <mason.r@northeastern.edu>
2025-06-10 17:22:29 -0400

    rename

--- Kambaz/Modules/dao.js
+++ Kambaz/Modules/dao.js
@@ -1,20 +1,20 @@
-import Database from "../Database/index.js";
+import db from "../Database/index.js";
 import { v4 as uuidv4 } from "uuid";
 export function findModulesForCourse(courseId) {
-    const { modules } = Database;
+    const { modules } = db;
     return modules.filter((module) => module.course === courseId);
 }
 export function createModule(module) {
     const newModule = { ...module, _id: uuidv4() };
-    Database.modules = [...Database.modules, newModule];
+    db.modules = [...db.modules, newModule];
     return newModule;
 }
 export function deleteModule(moduleId) {
-    const { modules } = Database;
-    Database.modules = modules.filter((module) => module._id !== moduleId);
+    const { modules } = db;
+    db.modules = modules.filter((module) => module._id !== moduleId);
 }
 export function updateModule(moduleId, moduleUpdates) {
-    const { modules } = Database;
+    const { modules } = db;
     const module = modules.find((module) => module._id === moduleId);
     Object.assign(module, moduleUpdates);
     return module;
